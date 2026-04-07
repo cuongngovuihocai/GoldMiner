5056ddf8e68c25f36b37bf559487112202a5f630
cuongngovuihocai <cuongngovuihocai@gmail.com>
2026-04-07 19:52:40 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/GoldMiner/', // <-- DÒNG BẠN VỪA THÊM VÀO
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
